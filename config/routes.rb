9c4c75cfbbb977bfa94d4fe716026a3a5d08db18
mamadou sylla <m27sylla@gmail.com>
2019-08-13 16:42:05 +0200

    exercice 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get "tasks", to: "tasks#index"
+  # create
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  # read one task
+  get "tasks/:id", to: "tasks#show"
+  # edit
+  get "tasks/:id/edit", to: "tasks#edit", as:"edit_task"
+  patch "tasks/:id", to: "tasks#update" , as:"task"
+  # delete
+  delete "tasks/:id", to: "tasks#destroy" ,as:""
 end
